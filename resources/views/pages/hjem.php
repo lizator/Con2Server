858f21ce0901083d38cb3f70f95ca55e520daac0
lizator <frederikkoefoeds@gmail.com>
2021-01-17 19:39:43 +0100

    conline 2021 update

--- resources/views/pages/hjem.php
+++ resources/views/pages/hjem.php
@@ -1,29 +1,19 @@
 <div class="hero">
-  <h2>Con2 2021:</h2>
-  <h1>HypoCon2dria!</h1>
+  <!--<h2>Con2 2021:</h2>
+  <h1>HypoCon2dria!</h1>-->
+    <h2>En lille, møghyggelig rollespilscon.</h2>
+    <h1>Con2</h1>
 
-  <div class="wholesome_msg_container">
+
+    <div class="wholesome_msg_container">
       <div class="wholesome_msg">
-          <img src="../../../img/content/pontifexkaninus.jpg">
-          <h2>Vigtig melding til kaninfolket!</h2>
 
-          <h3>Kære kaniner</h3>
+          <h2>Kalder alle søde coniner!</h2>
 
-          <p style="margin-bottom: 16px">Det er rigtig vigtigt for os at Con2 forbliver hyggelig og sikker for alle, så her er nogle Meget Seriøse og Mega Vigtige Ting:</p>
-
-          <p style="margin-bottom: 8px"><b>Næste Con2 bliver en 1 dags Con.</b> <br>Altså er der ingen overnatning og dørene åbner kun om lørdagen, hvilket nok bliver lørdag d. 30. januar. Mere information om programmet kommer når tilmeldingen åbner.</p>
-
-          <p style="margin-bottom: 8px"><b>Deltagerantallet bliver begrænset til cirka 40 deltagere. </b> <br>Derudover opfordrer vi til fashionable masker, så sørger vi for nok håndsprit til at desinficere en hel kaninfarm! :)</p>
-
-          <p style="margin-bottom: 8px">Vi er ved at finde ud af hvad vi gør med brætspil, figurspil og mad for at baktusser og sygdom kun er noget vi leger i temaet. Opdateringer vil komme ud på vores <a href="https://www.facebook.com/Con2.dk/">facebookside</a>, hjemmeside og på mail (<a href="mailto:mail@con2.dk?subject=Tilmeld mig maillisten!">mail@con2.dk</a>), når vi kommer tættere på tilmeldingen.</p>
-
-          <p style="margin-bottom: 8px">Hvis du har bekymringer, ros eller idéer (hvis du eksempelvis kan skaffer os temperaturmålere, de er fucking seje) så er du også velkommen til at skrive til os anytime!</p>
-
-          <p style="margin-bottom: 8px">Vi formåede at blive den eneste con i  2020, og nu arbejder vi på at give jer endnu en af de bedste og hyggeligste conner i 2021. <3</p>
-
-          <p>Mange corona-sikre kaninkrammere</p>
-          <h3>Con2 arrangørholdet</h3>
-          <img src="../../../img/content/team_pic.jpg" style="float: none; margin-left: 8rem;">
+          <p style="margin-bottom: 16px">Con2-arrangørerne har (på behørig afstand) omfavnet idéen om noget online hygge. Dette gør vi lørdag d. 30/1 vha. <b>Discord</b>! Vi har sørget for 2 scenarier og 2 spilblokke samt lidt hygge imellem.</p>
+          <p style="margin-bottom: 8px">Kom endelig og kom glade, og <b>har I lyst til at hjælpe som spilmester</b>, om det er jeres eget scenarie eller en af de planlagte, er I meget velkomne, og vi lover at give jeres navn en sej farve på serveren.</p>
+          <p style="margin-bottom: 8px">Link til <a href="https://fb.me/e/PvZ9KF3d">Facebook</a></p>
+          <p style="margin-bottom: 8px">Vi glæder til at se jer <b>CON</b>line</p>
 
       </div>
   </div>
@@ -36,11 +26,11 @@
       </div>
   </div> -->
 
-  <div class="hero_text">
+  <!-- <div class="hero_text">
     <h2>Afstand, Sprit og ikke mindst, Forskning<br>På med Kitlen og op med kaninørerne!</h2>
     <p>Vi i teamet arbejder på en behandling af dit con2-behov, og vi står snart med en plan, til hvordan vi kan komme igennem con2 2021 sikkert! Flere meldinger kommer snart lige her, samt på <a href="mailto:mail@con2.dk?subject=Tilmeld mig maillisten!">mail</a> og <a href="https://www.facebook.com/Con2.dk/">facebook</a> </p>
     <img src="../../../img/content/surgery_in_progress.jpg">
-  </div>
+  </div> -->
 
   <!-- <div class="btn-container" style="margin-bottom: 30px">
   <a class="btn btn-primary btn-lg" role="button" href="/tilmeld">Lige det job jeg vil have!</a>
