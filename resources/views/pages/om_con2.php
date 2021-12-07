a12e48d399d138799a0b89b816cf4cbfaecb1286
lizator <frederikkoefoeds@gmail.com>
2021-12-07 11:16:26 +0100

    Revert "Revert "Ready from moseCONnen brygger 2""
    This reverts commit 790912f3

---
+++ resources/views/pages/om_con2.php
@@ -0,0 +1,23 @@
+<div class="subpage_heading">
+  <h1>Hvem er vi?!</h1>
+</div>
+<div class="subpage_img">
+  <img alt="Nothing" class="activity_img" src="../../../img/content/rabbit_family.jpg">
+</div>
+
+<div class="subpage_content">
+    <h4>Generelt</h4>
+    <p>Ja, undskyld vi bliver en smule philosofiske, men vi føler nu det er et vigtigt spørgsmål at besvare.</p>
+
+    <p><b>Con2</b> er vores lille og hyggelige rollespilsCon, der består af rollespil, workshops, firgur spil, brætspil, god mad og massere af dårlig humor.</p>
+    <p>Eventet foregår fra fredag til og med søndag sent i januar eller tidligt i februar.</p>
+
+    <h4>Hvem er vi?</h4>
+    <p>Vi er 5 seje kaniner i bestyrelsen, der står for at organisere eventet og sørger for, at der bliver bestilt en overflod af gulerødder og porrer</p>
+    <p>men uden alle jer der møder op, laver mad, er spille mestre og gennerelt støtter op om vores lille con, ville vi ikke være hvor vi er nu <3</p>
+    <p>Vi glæder os til at se jer igen, og igen, og igen...</p>
+
+    <h4>Links og lignende</h4>
+    <p>Du kan følge med i hvad vi går og laver her på hjemmesiden, eller du kan følge os på <a href="https://www.facebook.com/Con2.dk/">Facebook</a></p>
+    <p>Og du kan finde Con2 foreningens vedtægter <a href="../../../pdf/Con2_Foreningsvedtgter.pdf">her</a></p>
+</div>
--- resources/views/pages/tilmeld.php
+++ resources/views/pages/tilmeld.php
@@ -1,26 +1,32 @@
 <div class="subpage_heading">
-  <h1>Vil du være med på teamet?</h1>
+  <h1>Skal du med på eventyr?</h1>
 </div>
 <div class="subpage_img">
-  <img class="activity_img" src="../../../img/content/health_anxiety.jpg">
+  <img alt="Nothing" class="activity_img" src="../../../img/content/happy_banker.jpg">
 </div>
 
 <div class="subpage_content">
-  <h2>Vi venter i spænding!</h2>
+  <!--<h2>Vi venter i spænding!</h2>
 
-  <p>Nu er der ikke længe før vi kan åbne dørene op for jer til endnu et Con2!</p>
+  <p>Tilmeldingen kommer når vi kommer tættere på eventet</p>
 
-  <p>Følg med på <a href="https://www.facebook.com/Con2.dk/">facebook</a> og på <a href="mailto:mail@con2.dk?subject=Tilmeld mig maillisten!">mail</a></p>
+  <p>Følg med på <a href="https://www.facebook.com/Con2.dk/">facebook</a> og på <a href="mailto:mail@con2.dk?subject=Tilmeld mig maillisten!">mail</a></p> -->
 
-  <!--<p>Klik på knappen nedenfor, for at tilmelde dig Con2 2020 - ConTOr! Husk at vi som altid refunderer folk der kommer i udklædning med 1 krone og evig påskønnelse!</p>
+    <h2>Tilmeldingen er her!</h2>
 
-  <p>PS: Når du har tilmeldt dig kan du evt. kigge på betalings-siden for at finde ud af hvor meget du skylder vores grådige leprechaun i banken. Vi sender dig også en mail når vi selv har haft mulighed for at gennemgå din tilmelding og tælle sammen. Husk: Vi opkræver ekstra hvis du betaler i døren, så sørg for at betale før du kommer ;)</p>
+  <p>Klik på knappen nedenfor, for at tilmelde dig MoseCONnen brygger 2! Husk at vi som altid refunderer folk der kommer i udklædning med 1 krone og evig påskønnelse!</p>
+
+    <p>grundet omstændighederne har vi kun 50 pladser. Dette betyder at hvis tilmeldingslinket ikke virker, er vi nok løbet tør for pladser.</p>
+
+  <p>PS: Når du har tilmeldt dig kan du evt. kigge på betalings-siden for at finde ud af hvor meget du skylder vores grådige leprechaun i banken.
+      Vi sender dig også en mail når vi selv har haft mulighed for at gennemgå din tilmelding og tælle sammen.
+      Husk: Vi opkræver ekstra hvis du betaler i døren, så sørg for at betale før du kommer ;)</p>
 
   <div class="btn-container">
     <span>
-      <a class="btn btn-primary btn-lg" role="button" href="https://docs.google.com/forms/d/e/1FAIpQLSfwzrO9HvK61qxwfdPOVVRXoeaD1kgzk29v7kEgJLZXLL5xcg/viewform?usp=sf_link" target="_blank">Weee! &#8605;</a>
+      <a class="btn btn-primary btn-lg" role="button" href="https://docs.google.com/forms/d/e/1FAIpQLSe844SOrOxEhkEsoAJ64YPldOUVKyq0bijqTJys77mHpielVA/viewform?usp=sf_link" target="_blank">Weee! &#8605;</a>
     </span>
   </div>
 
-  <img src="../../../img/content/health_anxiety.jpg">-->
+  <img alt="Nothing" src="../../../img/content/bad_leprechaun.jpg">
 </div>
